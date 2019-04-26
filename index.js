5d0a5c757fafa34aa259a90cb0f398dca1543a59
Anders Windelhed <anders.windelhed@bonnierbroadcasting.com>
2019-04-26 10:18:44 +0200

    Sorting rules

--- index.js
+++ index.js
@@ -27,6 +27,9 @@ const eslintRules = {
     },
   },
   rules: {
+    'arrow-body-style': 'off', // We can choose to include the braces on one-liners if we think it makes the code more readable
+    'class-methods-use-this': 'off', // We tried to implement this but it caused problems. Leaving it off for now
+    'consistent-return': 'off',
     'comma-dangle': [
       'error', {
         arrays: 'always-multiline',
@@ -36,43 +39,33 @@ const eslintRules = {
         functions: 'never',
       },
     ],
+    'default-case': 'off',
+    'func-names': 'off',
+    'import/no-self-import': 'off', // We had problems with this rule. For example files named 'espress.js' etc. Turning it off for now
+    'jsx-a11y/label-has-associated-control': ['error', { assert: 'either' }], // We allow either nesting of htmlFor association
+    'no-multiple-empty-lines': ['error', { max: 1 }], // We only allow one empty line
+    'no-param-reassign': 'off', // We allow reassigning variables
+    'no-plusplus': 'off', // We allow the unary ++ and -- operators
+    'no-use-before-define': ['error', { functions: true, variables: false }], // We like to keep the most important functions at the top of the file
+    'object-curly-newline': 'off', // We don’t enforce strict formatting of objects
+    'react/forbid-prop-types': 'off', // We allow arrays and objects as proptypesv
+    'react/jsx-closing-bracket-location': 'off', // We don't enforce the location of the closing bracket
+    'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }], // We allow jsx in js-files
+    'react/jsx-first-prop-new-line': 'off', // We can decide how to format the props from case to case
+    'react/jsx-max-props-per-line': 'off', // We can decide how to format the props from case to case
+    'react/jsx-one-expression-per-line': 'off', // We allow multiple expressions per line for example <span>Sparade recept <SvgIcon name="recipe" /></span>
+    'react/no-danger': 'off', // Sometimes we need to use dangerouslySetInnerHTML
+    'react/self-closing-comp': 'off', // We don't requre components to be self closed. <div id="video-player"></div> is ok
+    'react/sort-comp': 'off', // We allow excpetions to this strict sorting of methods
     'space-before-function-paren': ['error', {
       anonymous: 'never',
       named: 'never',
       asyncArrow: 'always',
     }],
-    radix: ['error', 'as-needed'],
-    curly: ['error', 'multi-line', 'consistent'],
-    semi: ['error', 'never'],
-    'consistent-return': 'off',
-    'default-case': 'off',
-    'func-names': 'off',
-    'no-plusplus': 'off', // We allow the unary ++ and -- operators
-    'arrow-body-style': 'off', // We can choose to include the braces on one-liners if we think it makes the code more readable
-    'no-use-before-define': [
-      'error', {
-        functions: true, // We like to keep the most important functions at the top of the file
-        variables: false,
-      },
-    ],
-    'no-multiple-empty-lines': ['error', { // We only allow one empty line
-      max: 1,
-    }],
     'vars-on-top': 'off', // We declare variables when we first use them instead of at the top of the function
-    'class-methods-use-this': 'off', // We tried to implement this but it caused problems. Leaving it off for now
-    'no-param-reassign': 'off', // We allow reassigning variables
-    'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }], // We allow jsx in js-files
-    'object-curly-newline': 'off', // We don’t enforce strict formatting of objects
-    'react/self-closing-comp': 'off', // We don't requre components to be self closed. <div id="video-player"></div> is ok
-    'react/sort-comp': 'off', // We allow excpetions to this strict sorting of methods
-    'react/no-danger': 'off', // Sometimes we need to use dangerouslySetInnerHTML
-    'react/forbid-prop-types': 'off', // We allow arrays and objects as proptypesv
-    'react/jsx-one-expression-per-line': 'off', // We allow multiple expressions per line for example <span>Sparade recept <SvgIcon name="recipe" /></span>
-    'react/jsx-closing-bracket-location': 'off', // We don't enforce the location of the closing bracket
-    'react/jsx-first-prop-new-line': 'off', // We can decide how to format the props from case to case
-    'react/jsx-max-props-per-line': 'off', // We can decide how to format the props from case to case
-    'import/no-self-import': 'off', // We had problems with this rule. For example files named 'espress.js' etc. Turning it off for now
-    'jsx-a11y/label-has-associated-control': ['error', { assert: 'either' }], // We allow either nesting of htmlFor association
+    curly: ['error', 'multi-line', 'consistent'],
+    radix: ['error', 'as-needed'],
+    semi: ['error', 'never'],
 
     'jsx-a11y/label-has-for': 'off', // Depricated rule - remove when airbnb is updated
   },
